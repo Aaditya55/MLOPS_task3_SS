582aec6c4467d0ddf8f551f0a637f42e0fde3af5
Shyam Sulbhewar <56698436+shyamwin@users.noreply.github.com>
2020-06-01 12:49:38 +0530

    ok please work yar

--- mlopstask3.py
+++ mlopstask3.py
@@ -177,6 +177,10 @@ history=model.fit(X_train,y_train , epochs=10 , verbose=0) #aresok
 
 accuracy_variable=history.history['accuracy'][9]
 
+import os
+os.environ['ACCR']=accuracy_vairable
+
+
 
 # In[28]:
 
