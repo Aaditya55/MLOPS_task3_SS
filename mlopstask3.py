4609c22bfb8781ebbc2a9047106e1b0341514e84
Shyam Sulbhewar <56698436+shyamwin@users.noreply.github.com>
2020-06-01 13:14:13 +0530

    last commit

--- mlopstask3.py
+++ mlopstask3.py
@@ -1,117 +1,26 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-
-
 from parameters import *
 import pandas as pd
-
-
-# In[2]:
-
-
 dataset = pd.read_csv('Churn_Modelling.csv')
-
-
-# In[3]:
-
-
 dataset.columns
-
-
-# In[4]:
-
-
 y = dataset['Exited']
-
-
-# In[ ]:
-
-
-
-
-
-# In[5]:
-
-
 X = dataset[['CreditScore', 'Age', 'Tenure', 'Balance', 'NumOfProducts', 'HasCrCard',
        'IsActiveMember', 'EstimatedSalary']]
-
-
-# In[6]:
-
-
 geo = dataset['Geography']
-
-
-# In[7]:
-
 
 geo = pd.get_dummies(geo, drop_first=True )
 
-
-# In[8]:
-
-
 gender = dataset['Gender']
 
-
-# In[9]:
-
-
 gender = pd.get_dummies(gender, drop_first=True )
-
-
-# In[10]:
-
-
 X = pd.concat([X,gender,geo], axis=1)
-
-
-# In[11]:
-
-
 X.info()
-
-
-# In[12]:
-
-
 from keras.optimizers import Adam
-
-
-# In[13]:
-
-
 from keras import metrics
-
-
-# In[14]:
-
-
 from sklearn.model_selection import train_test_split
-
-
-# In[15]:
-
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
-
-
-# In[16]:
-
-
 from keras.models import Sequential
-
-
-# In[17]:
-
-
-
-
-# In[18]:
-
-
 from keras.layers import Dense
 
 import tensorflow as tf
@@ -129,46 +38,13 @@ model.add(Dense(units=6, input_dim=11, activation='relu' ))
 
 
 model.add(Dense(units=3, activation='relu'))
-
-
-# In[21]:
-
-
 #model.add(Dense(units=2, activation='relu'))
-
-
-# In[22]:
-
-
 #model.add(Dense(units=2,  activation='relu' ))
-
-
-# In[23]:
-
-
 model.add(Dense(units=1,  activation='relu' ))
-
-
-# In[24]:
-
-
-
-
-# In[25]:
-
 
 model.compile(optimizer=Adam(learning_rate=var_lr),loss='binary_crossentropy',metrics=['accuracy'])
 
-
-# In[26]:
-
-
 #model.summary()
-
-
-# In[34]:
-
-
 history=model.fit(X_train,y_train , epochs=10 , verbose=0) #aresok
 
 
@@ -179,10 +55,5 @@ accuracy_variable=history.history['accuracy'][9]
 
 import os
 os.environ['ACCR']=str(accuracy_variable)  
-os.system("$ACCR= {0}".format(str(accuracy_variable))
-
-
-
-# In[28]:
-
+os.system("$ACCR= {0}".format(str(accuracy_variable)))
 
