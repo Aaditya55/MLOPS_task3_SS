fccd515dd94b369ca80677fd8e756688470daea0
Shyam Sulbhewar <56698436+shyamwin@users.noreply.github.com>
2020-06-01 13:42:49 +0530

    Update mlopstask3.py

--- mlopstask3.py
+++ mlopstask3.py
@@ -56,5 +56,5 @@ accuracy_variable=history.history['accuracy'][9]
 import os
 os.environ['ACCR']=str(accuracy_variable)  
 #os.system("ACCR= {0}".format(str(accuracy_variable)))
-print("Accuracy : ",accuracy_variable)
+print(accuracy_variable)
 
